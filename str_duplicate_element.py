d0fa2197dae03a24392c13f650b83aa7d6016030
Ahmedjam24 <30750847+Ahmedjam24@users.noreply.github.com>
2019-02-12 16:18:55 +0530

    Update str_duplicate_element.py

--- str_duplicate_element.py
+++ str_duplicate_element.py
@@ -2,15 +2,19 @@ usr_i=raw_input()
 usr=raw_input()
 usr=usr.split()
 end=int(usr_i)
+uni=0
 temp=0
 flag=200000
 for i in range (0,end):
     start=i+1
     for j in range(start,end):
         if(usr[i]==usr[j]):
+            uni=1
             if(flag!= usr[j]):
                 if (temp != 0):
                     print" "
                 print usr[i]
                 flag=usr[j]
                 temp=1
+if(uni==0):
+    print "unique"
