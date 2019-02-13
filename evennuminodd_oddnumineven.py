97440d7e3d70d6c8bc5552d4f90b8f304d5df70a
Ahmedjam24 <30750847+Ahmedjam24@users.noreply.github.com>
2019-02-13 15:39:09 +0530

    Create evennuminodd_oddnumineven.py

---
+++ evennuminodd_oddnumineven.py
@@ -0,0 +1,17 @@
+usr_i=raw_input()
+usr_i=int(usr_i)
+usr=raw_input()
+usr=usr.split()
+list=[]
+for i in range(0,usr_i):
+    if(i%2!=0):
+        if(int(usr[i])%2==0):
+            list.extend(usr[i])
+    else:
+        if(int(usr[i])%2!=0):
+            list.extend(usr[i])
+temp=0
+for i in list:
+    if(temp!=0):
+        print" "
+    print i
