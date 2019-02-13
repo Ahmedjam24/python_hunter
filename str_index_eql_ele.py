0d1f3c01a581d4afbcc493e68eb124ad7ed5eadd
Ahmedjam24 <30750847+Ahmedjam24@users.noreply.github.com>
2019-02-13 15:12:51 +0530

    Update str_index_eql_ele.py

--- str_index_eql_ele.py
+++ str_index_eql_ele.py
@@ -3,9 +3,13 @@ usr_i=int(usr_i)
 usr=raw_input()
 usr=usr.split()
 temp=0
+flag=0
 for i in range(0,usr_i):
     if(i==int(usr[i])):
         if(temp!=0):
             print" "
         print i
+        flag=1
         temp=1
+if (flag==0):
+    print "-1"
